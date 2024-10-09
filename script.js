a70402c277646849c141fbed07685db8aeb29a40
muskan <muskan8878h@gmail.com>
2024-10-09 13:11:11 +0530

    create a fun to delect the note

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const addBtn = document.querySelector("#addbtn")
-
+const main = document.querySelector("#main")
 addBtn.addEventListener(
     "click",
     function() {
@@ -20,9 +20,22 @@ const addNote = () => {
     note.classList.add("note");
     note.innerHTML = `
     <div class="tool">
-        <i class="fas fa-trash"></i>
-        <i class="fas fa-save"></i>
+        <i class="trash fas fa-trash"></i>
+        <i class="save fas fa-save"></i>
     </div>
     <textarea></textarea>
     `;
+
+    // to delect the note
+    note.querySelector(".trash").addEventListener(
+        "click",
+        function(){
+            note.remove();
+        }
+    )
+
+    
+
+    // to add the note 
+    main.appendChild(note); 
 }
