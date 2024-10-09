4252a67b50d6c6acdfbbee98d30432ad66e1c707
muskan <muskan8878h@gmail.com>
2024-10-09 13:04:20 +0530

    not using markep code we create js code

--- script.js
+++ script.js
@@ -7,6 +7,22 @@ addBtn.addEventListener(
     }
 )
 
+
+// <div class="note">
+//     <div class="tool">
+//         <i class="fas fa-trash"></i>
+//         <i class="fas fa-save"></i>
+//     </div>
+//     <textarea></textarea>
+// </div> 
 const addNote = () => {
-    
+    const note = document.createElement("div");
+    note.classList.add("note");
+    note.innerHTML = `
+    <div class="tool">
+        <i class="fas fa-trash"></i>
+        <i class="fas fa-save"></i>
+    </div>
+    <textarea></textarea>
+    `;
 }
